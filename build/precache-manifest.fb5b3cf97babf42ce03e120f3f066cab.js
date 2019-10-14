6239a79517caccff972033a6039c8ec78894decc
Milan Radojevic <mikister2012@gmail.com>
2019-10-14 19:09:56 +0200

    Add build

---
+++ build/precache-manifest.fb5b3cf97babf42ce03e120f3f066cab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c2007144f95f2623fc631c8501c737",
+    "url": "/pomodoro-react-100daysofcode/index.html"
+  },
+  {
+    "revision": "d20f0cc4ed415beef0bf",
+    "url": "/pomodoro-react-100daysofcode/static/css/main.e15c916f.chunk.css"
+  },
+  {
+    "revision": "4c0da4d82183d4d35685",
+    "url": "/pomodoro-react-100daysofcode/static/js/2.a1cdc048.chunk.js"
+  },
+  {
+    "revision": "d20f0cc4ed415beef0bf",
+    "url": "/pomodoro-react-100daysofcode/static/js/main.8e497520.chunk.js"
+  },
+  {
+    "revision": "4dd1534ed42f99bea8d5",
+    "url": "/pomodoro-react-100daysofcode/static/js/runtime-main.6872a81a.js"
+  }
+]);
