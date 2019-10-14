fd35266708333250bee067652424bf6e1063e59b
Milan Radojevic <mikister2012@gmail.com>
2019-10-14 16:40:34 +0200

    Add layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>POMODORO</h1>
+
+      <div className="timer">
+        <div className="timer__circle"></div>
+        <div className="timer__counter">25:00</div>
+      </div>
     </div>
   );
 }
